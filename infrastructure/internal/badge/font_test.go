e85659d870333c7ca726ce76bb564c82562d0693
EuiSung <52964858+gowoonsori@users.noreply.github.com>
2022-02-18 14:48:52 +0900

    Add : vera sans font

--- infrastructure/internal/badge/font_test.go
+++ infrastructure/internal/badge/font_test.go
@@ -12,7 +12,7 @@ func Test_Arial_Font_read(t *testing.T) {
 func Test_Arial_Font_Measure(t *testing.T) {
 	//given
 	fd := getArialDrawer()
-	expect := defaultTextWidth
+	expect := defaultArialFontWidth
 
 	//when
 	singleD := fd.measureString("1")
@@ -23,8 +23,32 @@ func Test_Arial_Font_Measure(t *testing.T) {
 
 	//then
 	tests.AssertEqual(t, singleD, expect)
-	tests.AssertEqual(t, doubleD1, expect+6)
-	tests.AssertEqual(t, doubleD2, expect+6)
-	tests.AssertEqual(t, tripleD1, expect+12)
-	tests.AssertEqual(t, tripleD2, expect+12)
+	tests.AssertEqual(t, doubleD1, expect+arialWidthPerWord)
+	tests.AssertEqual(t, doubleD2, expect+arialWidthPerWord)
+	tests.AssertEqual(t, tripleD1, expect+arialWidthPerWord*2)
+	tests.AssertEqual(t, tripleD2, expect+arialWidthPerWord*2)
+}
+
+func Test_Vera_Font_read(t *testing.T) {
+	getVeraDrawer()
+}
+
+func Test_Vera_Font_Measure(t *testing.T) {
+	//given
+	fd := getVeraDrawer()
+	expect := defaultVeraFontWidth
+
+	//when
+	singleD := fd.measureString("1")
+	doubleD1 := fd.measureString("12")
+	doubleD2 := fd.measureString("68")
+	tripleD1 := fd.measureString("123")
+	tripleD2 := fd.measureString("485")
+
+	//then
+	tests.AssertEqual(t, singleD, expect)
+	tests.AssertEqual(t, doubleD1, expect+veraWidthPerWord)
+	tests.AssertEqual(t, doubleD2, expect+veraWidthPerWord)
+	tests.AssertEqual(t, tripleD1, expect+veraWidthPerWord*2)
+	tests.AssertEqual(t, tripleD2, expect+veraWidthPerWord*2)
 }
