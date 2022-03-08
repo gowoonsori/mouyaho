f68c4a5ee6d9fa558b5b987028df6977e10a89d4
EuiSung <52964858+gowoonsori@users.noreply.github.com>
2022-03-08 22:48:26 +0900

    Update: go mod

--- go.mod
+++ go.mod
@@ -1,23 +1,3 @@
 module likeIt
 
 go 1.17
-
-require (
-	github.com/go-redis/redis/v8 v8.11.4
-	github.com/golang/freetype v0.0.0-20170609003504-e2365dfdc4a0
-	github.com/stretchr/testify v1.7.0
-	golang.org/x/image v0.0.0-20211028202545-6944b10bf410
-	gorm.io/gorm v1.22.5
-)
-
-require (
-	github.com/cespare/xxhash/v2 v2.1.2 // indirect
-	github.com/davecgh/go-spew v1.1.1 // indirect
-	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
-	github.com/jinzhu/inflection v1.0.0 // indirect
-	github.com/jinzhu/now v1.1.4 // indirect
-	github.com/joho/godotenv v1.4.0 // indirect
-	github.com/pmezard/go-difflib v1.0.0 // indirect
-	github.com/stretchr/objx v0.1.0 // indirect
-	gopkg.in/yaml.v3 v3.0.0-20200313102051-9f266ea9e77c // indirect
-)
--- main.go
+++ main.go
@@ -1,50 +1,5 @@
 package main
 
-import (
-	"fmt"
-	"github.com/joho/godotenv"
-	"github.com/stretchr/testify/mock"
-	"likeIt/application"
-	"likeIt/domain"
-	"likeIt/domain/mocks"
-	"likeIt/interfaces"
-	"net/http"
-	"os"
-)
-
-var env string
-
-func init() {
-	if err := godotenv.Load(".env"); err != nil {
-		panic("Error Loading .env file")
-	}
-	env = os.Getenv("APP_ENV")
-	if err := godotenv.Load(".env." + env); err != nil {
-		panic(fmt.Sprintf("Error Loading .env.%s file", env))
-	}
-}
-
 func main() {
-	port := os.Getenv("APP_PORT")
 
-	// Repository 구현체 생성
-	var rr domain.ReactRepository
-	if env == "local" {
-		mrr := new(mocks.ReactRepository)
-		mrr.On("FindCountByBadgeId", mock.Anything).Return(0)
-		mrr.On("FindByBadgeIdAndUserId", mock.Anything, mock.Anything).Return(nil)
-		rr = mrr
-	}
-
-	// Service(UseCase) 구현체 생성
-	bs := application.NewLikeBadgeService(rr)
-
-	// Handler 생성
-	badge := interfaces.NewLikeBadgeHandler(bs)
-
-	// 라우터 생성
-	mux := http.NewServeMux()
-	mux.HandleFunc("/api/like-badge", badge.GetLikeBadge)
-
-	http.ListenAndServe(port, mux)
 }
