021b28d9b16b6c9017a107d844c112be6c6d738e
gowoonsori <gowoonsori97@naver.com>
2022-03-20 20:18:09 +0900

    Add: state encrypt/decrpty method for oauth

---
+++ auth/state.go
@@ -0,0 +1,45 @@
+package auth
+
+import (
+	"crypto/aes"
+	"crypto/cipher"
+	"encoding/hex"
+)
+
+func EncryptAES(pt, key []byte) string {
+	//pt = base64.URLEncoding.EncodeToString([]byte(pt))
+	c, err := aes.NewCipher(key)
+	if err != nil {
+		panic(err)
+	}
+
+	gcm, err := cipher.NewGCM(c)
+	if err != nil {
+		panic(err)
+	}
+
+	nonce := make([]byte, gcm.NonceSize())
+	ct := gcm.Seal(nonce, nonce, pt, nil)
+	return hex.EncodeToString(ct)
+}
+
+func DecryptAES(ct, key []byte) string {
+	ct, _ = hex.DecodeString(string(ct))
+	c, err := aes.NewCipher(key)
+	if err != nil {
+		panic(err)
+	}
+
+	gcm, err := cipher.NewGCM(c)
+	if err != nil {
+		panic(err)
+	}
+
+	nonceSize := gcm.NonceSize()
+	nonce, pct := ct[:nonceSize], ct[nonceSize:]
+
+	pt, err := gcm.Open(nil, nonce, pct, nil)
+
+	//orgin, _ := base64.URLEncoding.DecodeString(string(pt))
+	return string(pt)
+}
